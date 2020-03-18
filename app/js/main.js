bc36771394a6bb1830d9c98600127dfb81dae1dd
Kry-dev <yaroslavkryvda@gmail.com>
2020-03-19 00:55:51 +0200

    create new navigation menu

--- app/js/main.js
+++ app/js/main.js
@@ -578,3 +578,18 @@ $('.quantity').each(function() {
         spinner.find("input").trigger("change");
     });
 });
+
+//menu script
+function openSubMenu(evt, cityName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " active";
+};
