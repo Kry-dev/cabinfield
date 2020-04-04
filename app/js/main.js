fce24084f0bc4861c5a2e7badc65b2b6094b3764
Kry-dev <yaroslavkryvda@gmail.com>
2020-04-04 20:51:14 +0300

    add steps to select options

--- app/js/main.js
+++ app/js/main.js
@@ -716,3 +716,43 @@ new Mmenu(
 $('mobile-close').on( "click", function() {
     api.close();
 });
+
+function bootstrapTabControl(){
+    let i, items = $('#productSelectTabs > li > .nav-link'), pane = $('#productSelectTabsContent > .tab-pane');
+    // next
+    $('.nexttab').on('click', function(){
+        for(i = 0; i < items.length; i++){
+            if($(items[i]).hasClass('active') == true){
+                break;
+            }
+        }
+        console.log(i);
+        if(i < items.length - 1){
+            // for tab
+            $(items[i]).removeClass('active');
+            $(items[i+1]).addClass('active');
+            // for pane
+            $(pane[i]).removeClass('show active');
+            $(pane[i+1]).addClass('show active');
+        }
+        
+    });
+    // Prev
+    $('.prevtab').on('click', function(){
+        for(i = 0; i < items.length; i++){
+            if($(items[i]).hasClass('active') == true){
+                break;
+            }
+        }
+        if(i != 0){
+            // for tab
+            $(items[i]).removeClass('active');
+            $(items[i-1]).addClass('active');
+            // for pane
+            $(pane[i]).removeClass('show active');
+            $(pane[i-1]).addClass('show active');
+        }
+    });
+}
+bootstrapTabControl();
+
