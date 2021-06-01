d88bdbc5a7a207dfbdab5fafdeba95e81cb5977a
Kry-dev <yaroslavkryvda@gmail.com>
2021-06-01 03:49:19 +0300

    top section

--- app/js/main.js
+++ app/js/main.js
@@ -163,23 +163,23 @@ $(document).ready(function () {
         }
     });
     
-    // /*show top-section*/
-    // if ( $(".top-section.d-hidden").length > 0 && $(".content .product").length > 0 ){
-    // 	$(window).scroll(function(){
-    // 		var scroll = $(window).scrollTop();
-    // 		var customize_product = $(".product").offset().top;
-    // 		if ( scroll > customize_product + 150 ){
-    // 			if ( $(".top-section.d-hidden").css("display") == "none" )
-    // 				$(".top-section.d-hidden").stop().slideDown(300);
-    // 		}
-    // 		else if ( $(".top-section.d-hidden").css("display") == "flex" ) {
-    // 			$(".top-section.d-hidden").stop().slideUp(200);
-    // 		}
-    // 		else{
-    // 			$(".top-section.d-hidden").stop().slideUp(200);
-    // 		}
-    // 	});
-    // }
+    /*show top-section*/
+    if ( $(".top-section.d-hidden").length > 0 && $(".content .product").length > 0 ){
+    	$(window).scroll(function(){
+    		var scroll = $(window).scrollTop();
+    		var customize_product = $(".product").offset().top;
+    		if ( scroll > customize_product + 150 ){
+    			if ( $(".top-section.d-hidden").css("display") == "none" )
+    				$(".top-section.d-hidden").stop().slideDown(300);
+    		}
+    		else if ( $(".top-section.d-hidden").css("display") == "flex" ) {
+    			$(".top-section.d-hidden").stop().slideUp(200);
+    		}
+    		else{
+    			$(".top-section.d-hidden").stop().slideUp(200);
+    		}
+    	});
+    }
     
     //Product image modal
     function product_modal_img(current_item) {
