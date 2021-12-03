32628321e90a817b23893f0d443a95d24f41f260
Kry-dev <yaroslavkryvda@gmail.com>
2021-12-03 20:14:24 +0200

    fix modal/ update links to button, which called the modal

--- app/js/main.js
+++ app/js/main.js
@@ -974,5 +974,11 @@ $(document).ready(function () {
         openGalleryPopup();
     });
     
+    $('.modal').on('show.bs.modal', function (e) {
+        var button = e.relatedTarget;
+        if($(button).hasClass('no-modal')) {
+            e.stopPropagation();
+        }
+    });
 });
 
